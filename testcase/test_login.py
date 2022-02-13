5d4a857d40f7e138b537e15742336b358fd513f0
zhangzhimeng <523504118@qq.com>
2022-02-14 00:45:11 +0800

    加了点东西

--- testcase/test_login.py
+++ testcase/test_login.py
@@ -1,16 +1,20 @@
-import os
-
+import requests
 import pytest
-from utils.YamlUtil import YamlReader
-from config.conf_dir import testdata_dir
-print(testdata_dir + os.sep + "login.yaml")
-test_data = YamlReader(testdata_dir + os.sep + "login.yaml").get_data()
-print(test_data)
+from common.base import Base
+
+
+base = Base()
+test_data = base.get_test_data("login")
+url = base.get_url() + test_data[0]["url"]
 
 
 @pytest.mark.parametrize("data", test_data)
 def test_login(data):
-    print("111111", data)
+    response = requests.post(url=url, data=data["data"]).json()
+    print(response)
+    assert response == data["expect"]
 
 
+if __name__ == '__main__':
+    print(url)
 
